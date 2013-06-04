3bf14ce1f5ade169fb393fe4733c3d9090d223e5
dcashman <dan.a.cashman@gmail.com>
2013-06-03 18:38:40 -0700

    Removed 1 byte get_random_bytes call for comparitive figure in paper

--- linux-2.6.32.57/net/ipv4/route.c
+++ linux-2.6.32.57/net/ipv4/route.c
@@ -887,8 +887,9 @@ static void rt_worker_func(struct work_struct *work)
 static void rt_cache_invalidate(struct net *net)
 {
 	unsigned char shuffle;
-
-	get_random_bytes(&shuffle, sizeof(shuffle));
+	
+	/* dacashman removed due to rand pollution */
+	//get_random_bytes(&shuffle, sizeof(shuffle));
 	atomic_add(shuffle + 1U, &net->ipv4.rt_genid);
 }
 
